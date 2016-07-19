f4bdc39ee23236ce96c2c2c2b836ecac6674fcfc
k04 <k04@k14>
2016-07-19 15:49:26 +0800

    接口示例

--- src/dao/TariffDao.java
+++ src/dao/TariffDao.java
@@ -1,5 +1,18 @@
 package dao;
 
-public interface TariffDao {
+import java.util.List;
 
+import bean.Tariff;
+
+public interface TariffDao {
+	
+	public List<Tariff> findAll();
+	
+	public Tariff findOne(int tariffId);
+	
+	public boolean add(Tariff tariff);
+	
+	public boolean del(int tariffId);
+	
+	public boolean update(Tariff tariff);
 }
