923254d9dba8fd04ef3dae8027d6110518b78153
k04 <k04@k15>
2016-07-19 16:46:42 +0800

    555

--- src/dao/impl/OsDaoImpl.java
+++ src/dao/impl/OsDaoImpl.java
@@ -1,5 +1,41 @@
 package dao.impl;
 
-public class OsDaoImpl {
+import java.util.List;
+
+import bean.Os;
+import dao.OsDao;
+
+public class OsDaoImpl implements OsDao {
+
+	@Override
+	public List<Os> findAll() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public Os findOne(int osId) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public boolean add(Os os) {
+		// TODO 自动生成的方法存根
+		return false;
+	}
+
+	@Override
+	public boolean del(int osId) {
+		// TODO 自动生成的方法存根
+		return false;
+	}
+
+	@Override
+	public boolean update(Os os) {
+		// TODO 自动生成的方法存根
+		return false;
+	}
+	
 
 }
