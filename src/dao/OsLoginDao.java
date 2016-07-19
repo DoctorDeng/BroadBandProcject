86a433a2c1feb8d54c099699b4f8b2c1d178e5e3
k04 <k04@k16>
2016-07-19 16:50:58 +0800

    接口方法注释

--- src/dao/OsLoginDao.java
+++ src/dao/OsLoginDao.java
@@ -7,13 +7,27 @@ import bean.Oslogin;
 public interface OsLoginDao {
 
 	public List<Oslogin> findAll();
+	/*
+	 * 查找所有os账号
+	 */
 	
 	public Oslogin findOne(int osloginId);
+	/*
+	 * 查找一个os账号
+	 */
 	
 	public boolean add(Oslogin oslogin);
-	
+	/*
+	 * 添加一个os账号
+	 */
 	public boolean del(int osloginId);
+	/*
+	 * 删除一个os账号
+	 */
 	
 	public boolean update(Oslogin oslogin);
+	/*
+	 * 修改os账号
+	 */
 	
 }
