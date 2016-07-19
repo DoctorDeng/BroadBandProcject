e1756a6782aa14f3c4c23a9edc64a072b4cc445a
k04 <k04@k15>
2016-07-19 16:52:03 +0800

    TariffDaoImpl

--- src/dao/impl/TariffDaoImpl.java
+++ src/dao/impl/TariffDaoImpl.java
@@ -1,5 +1,40 @@
 package dao.impl;
 
-public class TariffDaoImpl {
+import java.util.List;
+
+import bean.Tariff;
+import dao.TariffDao;
+
+public class TariffDaoImpl implements TariffDao {
+
+	@Override
+	public List<Tariff> findAll() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public Tariff findOne(int tariffId) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public boolean add(Tariff tariff) {
+		// TODO 自动生成的方法存根
+		return false;
+	}
+
+	@Override
+	public boolean del(int tariffId) {
+		// TODO 自动生成的方法存根
+		return false;
+	}
+
+	@Override
+	public boolean update(Tariff tariff) {
+		// TODO 自动生成的方法存根
+		return false;
+	}
 
 }
