cba08f8473df036af478fafcf197fed65864f95e
Administrator <Administrator@PC-20160219CHQI>
2016-07-19 16:46:20 +0800

    Billdao和billdetaildao的实现接口

--- src/dao/impl/BillDaoImpl.java
+++ src/dao/impl/BillDaoImpl.java
@@ -1,5 +1,40 @@
 package dao.impl;
 
-public class BillDaoImpl {
+import java.util.List;
+
+import bean.Bill;
+import dao.BillDao;
+
+public class BillDaoImpl implements BillDao{
+
+	@Override
+	public List<Bill> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bill findOne(int billId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Bill bill) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean del(int billId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Bill bill) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
