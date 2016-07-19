ec7ae308532ec2d076d003e70638f725858c2861
Administrator <Administrator@PC-20160219CHQI>
2016-07-19 16:41:18 +0800

    实现Adminpower和customer的接口

--- src/dao/impl/CustomerDaoImpl.java
+++ src/dao/impl/CustomerDaoImpl.java
@@ -1,5 +1,40 @@
 package dao.impl;
 
-public class CustomerDaoImpl {
+import java.util.List;
+
+import bean.Customer;
+import dao.CustomerDao;
+
+public class CustomerDaoImpl implements CustomerDao {
+
+	@Override
+	public List<Customer> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findOne(int CustomerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Customer customer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean del(Customer customer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Customer customer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
