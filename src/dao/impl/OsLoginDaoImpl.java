4ac26eacea15666c9e7a6b76c6b0c268c6ac5154
k04 <k04@k16>
2016-07-19 16:31:52 +0800

    osloginImpl

---
+++ src/dao/impl/OsLoginDaoImpl.java
@@ -0,0 +1,40 @@
+package dao.impl;
+
+import java.util.List;
+
+import bean.Oslogin;
+import dao.OsLoginDao;
+
+public class OsLoginDaoImpl implements OsLoginDao {
+
+	@Override
+	public List<Oslogin> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Oslogin findOne(int osloginId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Oslogin oslogin) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean del(int osloginId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Oslogin oslogin) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
