40be19d12e453ee9c65d2aef8dffd7b607749e6e
wym <wym@wym-PC.mshome.net>
2016-07-19 16:45:49 +0800

    adminDao和BusinessDao接口实现

--- src/dao/impl/AdminDaoImpl.java
+++ src/dao/impl/AdminDaoImpl.java
@@ -1,7 +1,27 @@
 package dao.impl;
 
-public class AdminDaoImpl {
+import java.util.List;
+
+import bean.Admin;
+import dao.AdminDao;
+
+public class AdminDaoImpl implements AdminDao{
+    
+	public List<Admin> findAll(){
+		return null;
+	}
 	
+	public Admin findOne(int adminId){
+		return null;
+	}
+	
+	public boolean add(Admin admin){
+		return false;
+	}
+	
+	public boolean update(Admin admin){
+		return false;
+	}
 	
 	
 }
