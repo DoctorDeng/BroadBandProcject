413903a829200cb95999df495a45157f4c5e0118
Joyzen <Joyzen@DESKTOP-64100S3>
2016-07-21 01:10:27 +0800

    aa

--- src/service/TariffManage.java
+++ src/service/TariffManage.java
@@ -1,5 +1,27 @@
 package service;
 
+import java.util.List;
+
+import bean.Tariff;
+import dao.impl.TariffDaoImpl;
+
 public class TariffManage {
 
+	/**
+	 * 获取资费管理所需要的表格信息（后期可能需要封装到pageBean里面，暂时封装到Tariff）
+	 * @return
+	 */
+	public List<Tariff> getShowMessage(){
+		return new TariffDaoImpl().findAll();
+	}
+	
+	/**
+	 * 增加资费
+	 * @param t
+	 * @return
+	 */
+	public boolean addTariff(Tariff t){
+		return new TariffDaoImpl().add(t);
+	}
+	
 }
