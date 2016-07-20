e9bd03d665ef34a13ad8c6766695c607542190fe
k04 <k04@k16>
2016-07-20 12:00:51 +0800

    BussinessDaoImpl

--- src/dao/impl/BussinessDaoImpl.java
+++ src/dao/impl/BussinessDaoImpl.java
@@ -1,17 +1,46 @@
 package dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import bean.Bussiness;
 import dao.BussinessDao;
+import util.DBHelper;
 
 public class BussinessDaoImpl implements BussinessDao{
+	
+	Connection conn = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
+	
+	
 	public List<Bussiness> findAll(){
-		return null;
+		List<Bussiness> l = new ArrayList<Bussiness>();
+		conn = DBHelper.getConnection();
+		try {
+			ps = conn.prepareStatement("select * from bussiness");
+			rs = ps.executeQuery();
+			while(rs.next()){
+				Bussiness b = new Bussiness();
+				b.setBussinessId(rs.getInt(1));
+				b.setAdminId(rs.getInt(2));
+				b.setOsId(rs.getInt(3));
+				b.setStatus(rs.getString(4));
+				l.add(b);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return l;
 	}
 	
 	public Bussiness findOne(int bussinessId){
-		return null;
+		return new Bussiness();
 	}
 	
 	public boolean add(Bussiness bussiness){
@@ -25,4 +54,5 @@ public class BussinessDaoImpl implements BussinessDao{
 	public boolean update(Bussiness business){
 		return false;
 	}
+	
 }
