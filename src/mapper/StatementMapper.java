0fe92e46443d2cc83be349f020df8e64fe277269
Administrator <Administrator@503-15>
2016-07-30 19:18:38 +0800

    修改配置文件

--- src/mapper/StatementMapper.java
+++ src/mapper/StatementMapper.java
@@ -2,9 +2,10 @@ package mapper;
 
 import java.util.List;
 
+import bean.Page;
 import bean.vo.StatementVo;
 
 public interface StatementMapper {
 	
-	public List<StatementVo> selectAllStatementByPagging();
+	public List<StatementVo> selectAllStatementByPagging(Page page);
 }
