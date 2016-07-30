87cbdc097afdbf00c7969076b59f9bc4a1e0b799
Administrator <Administrator@503-15>
2016-07-30 16:48:42 +0800

    配置修改

--- src/bean/vo/BillFormBean.java
+++ src/bean/vo/BillFormBean.java
@@ -16,7 +16,7 @@ public class BillFormBean {
 	/**
 	 * 账务账号
 	 */
-	private String loginAccount;
+	private String customerAccount;
 	/**
 	 * 总时长
 	 */
@@ -30,58 +30,5 @@ public class BillFormBean {
 	 */
 	private String payStatus;
 	
-	public BillFormBean(int billId, String customerName, String idNumber, String loginAccount, String timeLong,
-			 String payWay, String payStatus) {
-		this.billId = billId;
-		this.customerName = customerName;
-		this.idNumber = idNumber;
-		this.loginAccount = loginAccount;
-		this.timeLong = timeLong;
-		this.payWay = payWay;
-		this.payStatus = payStatus;
-	}
-	
-	public int getBillId() {
-		return billId;
-	}
-	public void setBillId(int billId) {
-		this.billId = billId;
-	}
-	public String getCustomerName() {
-		return customerName;
-	}
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-	public String getIdNumber() {
-		return idNumber;
-	}
-	public void setIdNumber(String idNumber) {
-		this.idNumber = idNumber;
-	}
-	public String getLoginAccount() {
-		return loginAccount;
-	}
-	public void setLoginAccount(String loginAccount) {
-		this.loginAccount = loginAccount;
-	}
-	public String getTimeLong() {
-		return timeLong;
-	}
-	public void setTimeLong(String timeLong) {
-		this.timeLong = timeLong;
-	}
-	public String getPayWay() {
-		return payWay;
-	}
-	public void setPayWay(String payWay) {
-		this.payWay = payWay;
-	}
-	public String getPayStatus() {
-		return payStatus;
-	}
-	public void setPayStatus(String payStatus) {
-		this.payStatus = payStatus;
-	}
 	
 }
