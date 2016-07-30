e65c91d84a509a4d7ca24c9768675ae80416e384
Administrator <Administrator@503-88>
2016-07-30 11:32:36 +0800

    Os  oslogin 完善

--- src/bean/Os.java
+++ src/bean/Os.java
@@ -2,4 +2,85 @@ package bean;
 
 public class Os {
 
+    private int osId;    //宽带账号ID
+    private int customerId;    //业务账号ID
+    private int tariffId;    //资费ID
+    private String osAccount;     //宽带账号
+    private String osPassword;     //宽带账号密码
+    private String serverIp;      //服务器IP地址
+    private Customer customer;   //客户对象
+    private Tariff  tarriff;     //资费对象
+    
+    
+    
+	public Os() {
+		super();
+	}
+	
+	public Os(int osId, int customerId, int tariffId, String osAccount, String osPassword, String serverIp) {
+		this.osId = osId;
+		this.customerId = customerId;
+		this.tariffId = tariffId;
+		this.osAccount = osAccount;
+		this.osPassword = osPassword;
+		this.setServerIp(serverIp);
+	}
+	
+
+	public int getOsId() {
+		return osId;
+	}
+	public void setOsId(int osId) {
+		this.osId = osId;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public int getTariffId() {
+		return tariffId;
+	}
+	public void setTariffId(int tariffId) {
+		this.tariffId = tariffId;
+	}
+	public String getOsAccount() {
+		return osAccount;
+	}
+	public void setOsAccount(String osAccount) {
+		this.osAccount = osAccount;
+	}
+	public String getOsPassword() {
+		return osPassword;
+	}
+	public void setOsPassword(String osPassword) {
+		this.osPassword = osPassword;
+	}
+	
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Tariff getTarriff() {
+		return tarriff;
+	}
+
+	public void setTarriff(Tariff tarriff) {
+		this.tarriff = tarriff;
+	}
+
+	public String getServerIp() {
+		return serverIp;
+	}
+
+	public void setServerIp(String serverIp) {
+		this.serverIp = serverIp;
+	}
+	
 }
--- src/bean/OsLogin.java
+++ src/bean/OsLogin.java
@@ -1,5 +1,62 @@
 package bean;
 
 public class OsLogin {
+	private  int osLoginId; 
+	private  int osId;
+	private  String LoginIp;
+	private  String loginInTime;
+	private  String loginOutTime;
+	private  int timeLong;
+	private  Os  os;
+	
+	public OsLogin(){
+		super();
+	}
+	
+	public int getOsLoginId() {
+		return osLoginId;
+	}
 
+	public void setOsLoginId(int osLoginId) {
+		this.osLoginId = osLoginId;
+	}
+
+	public int getOsId() {
+		return osId;
+	}
+	public void setOsId(int osId) {
+		this.osId = osId;
+	}
+	public String getLoginIp() {
+		return LoginIp;
+	}
+	public void setLoginIp(String loginIp) {
+		LoginIp = loginIp;
+	}
+	public String getLoginInTime() {
+		return loginInTime;
+	}
+	public void setLoginInTime(String loginInTime) {
+		this.loginInTime = loginInTime;
+	}
+	public String getLoginOutTime() {
+		return loginOutTime;
+	}
+	public void setLoginOutTime(String loginOutTime) {
+		this.loginOutTime = loginOutTime;
+	}
+	public int getTimeLong() {
+		return timeLong;
+	}
+	public void setTimeLong(int timeLong) {
+		this.timeLong = timeLong;
+	}
+	public Os getOs() {
+		return os;
+	}
+	public void setOs(Os os) {
+		this.os = os;
+	}
+	
+	
 }
