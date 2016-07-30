812a38ac33def402bd6431f57787ea070f4cec55
Administrator <Administrator@503-88>
2016-07-30 11:55:47 +0800

    OSLoginmapper接口

---
+++ src/mapper/OsLoginMapper.java
@@ -0,0 +1,14 @@
+package mapper;
+
+import java.util.List;
+
+import bean.OsLogin;
+
+public interface OsLoginMapper {
+	/**
+	 * 通过某一个值来查询，此处的By后面值需要有需要的来填写
+	 * @param OsLogin
+	 * @return
+	 */
+	public List<OsLogin> selOsLoginDaoBy(OsLogin OsLogin);
+}
